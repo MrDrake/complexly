9f1ab1282c7e96d384eeb86bfae7013c6b2cb53b
Mr. Drake <john@mrdrake.net>
2018-10-15 16:28:21 +1100

    Added run code functionalilty

--- generators/javascript/text.js
+++ generators/javascript/text.js
@@ -280,7 +280,8 @@ Blockly.JavaScript['text_print'] = function(block) {
   // Print statement.
   var msg = Blockly.JavaScript.valueToCode(block, 'TEXT',
       Blockly.JavaScript.ORDER_NONE) || '\'\'';
-  return 'window.alert(' + msg + ');\n';
+  /* return 'window.alert(' + msg + ');\n'; */
+  return '$("#console").append("<p>"+'+msg+'+"</p>");\n';
 };
 
 Blockly.JavaScript['text_prompt_ext'] = function(block) {
